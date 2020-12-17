935bdb9cc20acf7f89c2abee747f0af0c6810d79
James Totah <jtotah@usfca.edu>
2020-12-17 13:30:46 -0800

    UPDATE: Added api routes to serve style and script for notes.html

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { text } = require("express");
 const fs = require("fs");
 
 router.get("/notes", (req, res) => {
@@ -8,6 +9,23 @@ router.get("/notes", (req, res) => {
     })
     console.log("get /api/notes successfull!");
 });
+
+router.get("/notes/script", (req, res) => {
+    fs.readFile("./public/assets/js/index.js", "utf8", (err, data) => {
+        if (err) throw err;
+        res.type('.js');
+        res.send(data);
+    })
+})
+
+router.get("/notes/css", (req, res) => {
+    fs.readFile("./public/assets/css/styles.css", "utf8", (err, data) => {
+        if (err) throw err;
+        res.type('.css');
+        res.send(data);
+    })
+})
+
 
 router.post("/notes")
 
