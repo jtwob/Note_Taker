8a4f44dea26fe65e596a28c2f3429755cef2658a
James Totah <jtotah@usfca.edu>
2020-12-17 14:57:57 -0800

    UPDATE: Implemented post and delete requests

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { text } = require("express");
 const fs = require("fs");
+const uniqid = require("uniqid");
 
 router.get("/notes", (req, res) => {
     fs.readFile("./db/db.json", "utf8", (err, data) => {
@@ -27,6 +28,36 @@ router.get("/notes/css", (req, res) => {
 })
 
 
-router.post("/notes")
+router.post("/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        const allNotes = JSON.parse(data);
+        allNotes.push({
+            title: req.body.title,
+            id: uniqid(),
+            text: req.body.text,
+        });
+        fs.writeFile("./db/db.json", JSON.stringify(allNotes), (err) => {
+            if (err) throw err;
+            return res.json({ msg: "successfully added" });
+        });
+    });
+});
+
+router.delete("/notes/:id", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        const allNotes = JSON.parse(data);
+        for (let i = 0; i < allNotes.length; i++) {
+            if (allNotes[i].id === req.params.id) {
+                allNotes.splice(i, 1);
+            }
+        }
+        fs.writeFile("./db/db.json", JSON.stringify(allNotes), (err) => {
+            if (err) throw err;
+            return res.json({ msg: "successfully deleted" });
+        });
+    })
+})
 
 module.exports = router;
