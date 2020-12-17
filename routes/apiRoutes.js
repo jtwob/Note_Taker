8109bef34fd50e8fa9ee9ef960bb40c33f85c0b2
James Totah <jtotah@usfca.edu>
2020-12-17 12:46:45 -0800

    UPDATE: server.js given basic functionality, client and api routes in progress

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,14 @@
 const router = require("express").Router();
+const fs = require("fs");
 
-router.get("/", (req, res) => {
-    console.log("get / successfull!");
+router.get("/notes", (req, res) => {
+    fs.readFile("../db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    })
+    console.log("get /api/notes successfull!");
 });
+
+router.post("/notes")
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+const apiRoutes = require("./routes/apiRoutes");
+const clientRoutes = require("./routes/clientRoutes");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/api", apiRoutes);
+app.use("/", clientRoutes);
+
+app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
